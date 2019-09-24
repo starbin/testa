9ca90213f566b0bc40a18dd8cf17259b437337bd
bintangraga <bintanggiri.raga@gmail.com>
2019-09-24 18:56:12 +0700

    add grid scss

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,8 +5,22 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
 </head>
 <body>
-    Hello
+    <div class="container--fluid">
+        <div class="row">
+            <div class="grid__col-4" style="background: yellow;">
+                test
+            </div>
+            <div class="grid__col-4" style="background: red;">
+                test2
+            </div>
+            <div class="grid__col-4" style="background: black;">
+                test
+            </div>
+        </div>
+    </div>
 </body>
 </html>
