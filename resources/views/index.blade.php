8c54ad353e706c6bd22eaad9c0f99ba412d092ad
bintangraga <bintanggiri.raga@gmail.com>
2019-10-04 22:19:57 +0700

    add few tweaks

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -51,47 +51,22 @@
                                 </div>
                                 <div class="card__content">
                                     <div class="card__title">
-                                        afawfawfa
-                                    </div>
-                                    <div class="card__infos">
-                                        card infos
-                                    </div>
-                                </div>
-                                <div class="card__vote">
-                                    <div class="voter">
-                                        <div class="voter__wrapper">
-                                            <div class="voter__up">
-                                                <button class="voter__button"><i class="fa fa-angle-up"></i></button>
-                                            </div>
-                                            <div class="voter__sums">
-                                                200
-                                            </div>
-                                            <div class="voter__down">
-                                                <button class="voter__button"><i class="fa fa-angle-down"></i></button>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="card">
-                            <div class="card__wrapper">
-                                <div class="card__media" >
-                                    <figure class="card__image" role="img" style="background-image: url(https://via.placeholder.com/150)">
-                                        <img alt="media alt">
-                                    </figure>
-                                </div>
-                                <div class="card__content">
-                                    <div class="card__title">
-                                        afawfawfa
+                                        <a href="">
+                                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sit amet ex feugiat, tempus ex ullamcorper, faucibus arcu. Sed convallis dui vitae lectus gravida, at luctus eros pretium. 
+                                        </a>    
                                     </div>
                                     <div class="card__infos">
                                         <div class="card__poster">
-                                            posted by <a href="#">user/</a>
+                                            posted by <a href="#">orlandoMagic</a>
                                         </div>
                                     </div>
-                                    <div class="card__actions">
-                                        <a href="" class="card__share"><i class="fa fa-share"></i>share</a>
+                                    <div class="card__action">
+                                        <a href="" class="card__actionbutton card__comment">
+                                            <span class="card__actionicon"><i class="fa fa-envelope"></i></span>2,4K Replies
+                                        </a>
+                                        <a href="" class="card__actionbutton card__share">
+                                            <span class="card__actionicon"><i class="fa fa-share"></i></span>share
+                                        </a>
                                     </div>
                                 </div>
                                 <div class="card__vote">
